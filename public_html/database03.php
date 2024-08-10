111a175c2e7aaee4e743a6d30e5b1a02a8b2f2bd
joegenequ <joegenequesada@gmail.com>
2024-08-10 05:16:59 +0000

    mysql select statement

--- public_html/database03.php
+++ public_html/database03.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id, firstname, lastname, email, reg_date FROM MyGuests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]." " . $row["email"]." " . $row["reg_date"]. "<br>";
   }
 } else {
   echo "0 results";
