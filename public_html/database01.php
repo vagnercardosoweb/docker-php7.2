bd78a3a6352d9fa0a0febcb11fdc920e7a967ad4
joegenequ <joegenequesada@gmail.com>
2024-08-10 03:08:19 +0000

    created form03.php and mysql create database.

---
+++ public_html/database01.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+
+
+
+?>
